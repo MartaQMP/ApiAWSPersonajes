86e45086fb5b38c8d32353eeaea4879e91c60cd0
MartaQMP <martaquirosmp@gmail.com>
2026-05-19 11:47:31 +0200

    Add project files.

---
+++ ApiAWSPersonajes/Controllers/PersonajeController.cs
@@ -0,0 +1,32 @@
+﻿using ApiAWSPersonajes.Models;
+using ApiAWSPersonajes.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAWSPersonajes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajeController : ControllerBase
+    {
+        private RepositoryTelevision repo;
+
+        public PersonajeController(RepositoryTelevision repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Personaje>>> GetPersonajes()
+        {
+            return await this.repo.GetPersonajesAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Personaje personaje)
+        {
+            await this.repo.InsertPersonajeAsync(personaje.IdPersonaje, personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+    }
+}
---
+++ ApiAWSPersonajes/Repositories/RepositoryTelevision.cs
@@ -0,0 +1,40 @@
+﻿using ApiAWSPersonajes.Data;
+using ApiAWSPersonajes.Models;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Bcpg;
+
+namespace ApiAWSPersonajes.Repositories
+{
+    public class RepositoryTelevision
+    {
+        private readonly TelevisionContext context;
+
+        public RepositoryTelevision(TelevisionContext context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            return await this.context.Personajes.ToListAsync();
+        }
+
+        public async Task<int> GetMaxIdPersonajeAsync()
+        {
+            return await this.context.Personajes.MaxAsync(p => p.IdPersonaje);
+        }
+
+        public async Task InsertPersonajeAsync(int idPersonaje, string nombre, string imagen)
+        {
+            Personaje personaje = new Personaje
+            {
+                IdPersonaje = idPersonaje,
+                Nombre = nombre,
+                Imagen = imagen
+            };
+            await this.context.Personajes.AddAsync(personaje);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
