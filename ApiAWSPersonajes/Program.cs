db9daf11c3fc5a4c747c98d0593afe7054b7ef8b
MartaQMP <martaquirosmp@gmail.com>
2026-05-20 09:24:01 +0200

    20/05

--- ApiAWSPersonajes/Program.cs
+++ ApiAWSPersonajes/Program.cs
@@ -5,8 +5,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//?Add?services?to?the?container. 
-
 builder.Services.AddCors(p => p.AddPolicy("enabled", options =>
 {
     options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
